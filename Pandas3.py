b17f47b7742cd9b9f3c239121099ae3e7a80a169
Felipe Lima <50051490+felipelimapy@users.noreply.github.com>
2019-08-13 15:29:09 -0400

    Delete Pandas3.py

--- Pandas3.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df=pd.read_csv("dados.csv")
-#print (df["quartos"].plot.hist(bins=30,edgecolor='White'))
-print(df["bairro"].value_counts().plot.barh(title="Número de Apartamentos"))
